0e6b502710d26ee4a0e79dbdd4ded359c37a9908
chethankumarhm <chethankumarhmm@gmail.com>
2025-01-26 13:11:35 +0530

    new Register specfile

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -2,18 +2,20 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://demowebshop.tricentis.com/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle("Demo Web Shop");
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://demowebshop.tricentis.com/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  await page.locator("//a[text()='Register']").click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  const registerText = await page.locator("//h1[text()='Register']").textContent();
+  expect(registerText).toBe(registerText);
+
 });
